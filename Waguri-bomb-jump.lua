364876e22443a5ddc84bf29ae987e94f1992d0f8
waguri-mm2 <rahhood31@gmail.com>
2026-05-23 13:27:51 +0800

    Update script key and load new script URL

--- Waguri-bomb-jump.lua
+++ Waguri-bomb-jump.lua
@@ -1,3 +1,2 @@
-script_key = "trial"
-
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/9e07bb325727d06fe704c4002c7e3c3b4d4c9899fbf3aa2964116a5e4bc4e9d7.lua"))()
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e0c639b64210618f1e4cdcc21eb36a610a915131cf2c710cdfa5b1eff8c6b8de/download"))()
